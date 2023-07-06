57e245ff00c7b75a719c1fd127d906590b51ca3b
Ms.He mac <crabapples.cn@gmail.com>
2023-07-06 21:00:34 +0800

    修复springboot版本和jdk版本冲突问题

--- eureka-registry-service/src/main/java/cn/crabapples/eurekaregistryservice/EurekaApplication.java
+++ eureka-registry-service/src/main/java/cn/crabapples/eurekaregistryservice/EurekaApplication.java
@@ -2,12 +2,14 @@ package cn.crabapples.eurekaregistryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class EurekaRegistryServiceApplication {
+@EnableEurekaServer
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaRegistryServiceApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 
 }
