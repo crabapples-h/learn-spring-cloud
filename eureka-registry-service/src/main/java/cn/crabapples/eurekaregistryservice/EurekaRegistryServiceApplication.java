b8dc38ea3fc3ff02c9bdbf2c107b3bbcd64d7451
Ms.He mac <crabapples.cn@gmail.com>
2023-07-06 18:37:47 +0800

    first commit

---
+++ eureka-registry-service/src/main/java/cn/crabapples/eurekaregistryservice/EurekaRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package cn.crabapples.eurekaregistryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRegistryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaRegistryServiceApplication.class, args);
+    }
+
+}
