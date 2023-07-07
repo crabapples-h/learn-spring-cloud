03c0c77063899fc63646aa8f03cf321f9cf14787
mshe <crabapples.cn@gmail.com>
2023-07-08 02:18:52 +0800

    加入nacos注册中心

--- service-provider/src/main/java/cn/crabapples/ProviderApplication.java
+++ service-provider/src/main/java/cn/crabapples/ProviderApplication.java
@@ -2,10 +2,10 @@ package cn.crabapples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class ProviderApplication {
 
     public static void main(String[] args) {
