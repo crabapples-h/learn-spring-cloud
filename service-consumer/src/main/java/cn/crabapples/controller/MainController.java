a7466d05260bcd8dc4e5016bc161422084ee0abb
mshe <crabapples.cn@gmail.com>
2023-07-08 23:52:51 +0800

    加入自动刷新配置文件

--- service-consumer/src/main/java/cn/crabapples/controller/MainController.java
+++ service-consumer/src/main/java/cn/crabapples/controller/MainController.java
@@ -1,19 +1,42 @@
 package cn.crabapples.controller;
 
+import cn.crabapples.Test;
 import cn.crabapples.common.ResponseDTO;
 import cn.crabapples.service.MainService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/consumer")
 @Slf4j
+@RefreshScope
 public class MainController {
+    @Value("${test:666}")
+    private String test;
+
     private final MainService service;
 
-    public MainController(MainService service) {
+    private final Test test1;
+
+    public MainController(MainService service, Test test1) {
         this.service = service;
+        this.test1 = test1;
+    }
+
+    @RequestMapping("/test")
+    public ResponseDTO test() {
+        log.info("消费者收到请求");
+        return ResponseDTO.returnSuccess(test);
+    }
+
+    @RequestMapping("/test1")
+    public ResponseDTO test1() {
+        log.info("消费者收到请求");
+        return ResponseDTO.returnSuccess(test1);
     }
 
     @RequestMapping("/hello")
